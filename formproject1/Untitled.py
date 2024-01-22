ff2ecfc0a256d58b217cbad86d08e766a3cc80cd
madhurukku <madhavisukari265@gmail.com>
2024-01-22 14:50:05 +0530

    first commit

---
+++ formproject1/Untitled.py
@@ -0,0 +1,2 @@
+c=int(input("enter any number"))
+print("digits") if c>="0" and c<="9"  else print("alnum") if c>="a" and c<="z" and c>="A" and c<="Z" and c>="0" and c<="9" else print("special character")  
